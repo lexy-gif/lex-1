78d567899bfba57370b234f95de9ed149cb1afb8
Alex Kibaru <alex.kibaru@devopsfoundry.cloud>
2026-05-12 09:33:19 +0300

    Made the Application to be well containerized

--- includes/config.php
+++ includes/config.php
@@ -1,13 +1,16 @@
 <?php 
 // DB credentials.
-define('DB_HOST','db');
-define('DB_USER','srms_user');
-define('DB_PASS','srms_password');
-define('DB_NAME','srms');
+define('DB_HOST', getenv('DB_HOST') ?: 'db');
+define('DB_USER', getenv('DB_USER') ?: 'srms_user');
+define('DB_PASS', getenv('DB_PASS') ?: 'srms_password');
+define('DB_NAME', getenv('DB_NAME') ?: 'srms');
 // Establish database connection.
 try
 {
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS, array(
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'",
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+));
 }
 catch (PDOException $e)
 {
